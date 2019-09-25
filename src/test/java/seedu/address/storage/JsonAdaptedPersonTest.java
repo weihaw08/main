5429ebf4c07863d70791a30205004c181d40c4ae
weihaw08 <weihaw08@yahoo.com.sg>
2019-09-25 23:15:52 +0800

    Make minor edits to pass checkstyleTest

--- src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
+
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
